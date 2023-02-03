6945f21f20d020b525872b277c38c1958c38bcf6
euronymou1991 <124392477+euronymou1991@users.noreply.github.com>
2023-02-03 12:47:33 -0600

    Add files via upload

---
+++ EVA1_HOLA_MUNDO/src/eva1_hola_mundo/EVA1_HOLA_MUNDO.java
@@ -0,0 +1,14 @@
+package eva1_hola_mundo;
+
+
+public class EVA1_HOLA_MUNDO {
+
+
+    public static void main(String[] args) {
+        int x=100;
+        System.out.println("Hola Mundo!!");
+         System.out.println("x");
+        
+    }
+   
+}
