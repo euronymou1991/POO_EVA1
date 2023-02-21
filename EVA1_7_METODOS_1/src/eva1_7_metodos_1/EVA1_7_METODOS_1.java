23e1b30ff7e721b98e4125f24c2f74807400cdd5
euronymou1991 <124392477+euronymou1991@users.noreply.github.com>
2023-02-21 12:43:10 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,15 @@
+
+package eva1_7_metodos_1;
+
+public class EVA1_7_METODOS_1 {
+
+    
+    public static void main(String[] args) {
+        
+        myMethod();
+        
+    }
+    public static void myMethod(){
+        System.out.println("You have called me! My name is myMethod!!");
+    }
+}
