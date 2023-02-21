4282f88114c2407cdc0fbb5295f78c877e16f48a
euronymou1991 <124392477+euronymou1991@users.noreply.github.com>
2023-02-21 12:51:12 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/eva1_9_mensaje/EVA1_9_MENSAJE.java
@@ -0,0 +1,18 @@
+
+package eva1_9_mensaje;
+
+
+public class EVA1_9_MENSAJE {
+
+    
+    public static void main(String[] args) {
+        nPrintLn("Hola",5); //Llamada a funcion
+    }
+    
+    public static void nPrintLn (String message, int num){
+        for (int i=0; i<num;i++){
+            System.out.println("message");
+        }
+                
+    }
+}
